986d99ca54a3a934945ee44e4e4b1fad93a011b7
Finnian T. Murphy <finnianmurph@gmail.com>
2025-01-21 15:10:11 -0500

    Hopefully that worked

--- main.cpp
+++ main.cpp
@@ -1,27 +1,4 @@
-#include <iostream>
-#include <string>
-
-
-
-void termDisp() {
-
-bool dRun = true;
-while(dRun == true){
-std::string userInp = "";
-std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Continue? \n  (enter to continue, q to quit):\n > " << std::endl;
-std::cin >> userInp;
-
-if(userInp == "q"){
-dRun = false;
-
-} else {}
-
-
-
-}
-
-
-}
+#include "main.h"
 
 
 int main() {
