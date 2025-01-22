9554e994f70ec0257278670032854f0c49f9899c
Finnian T. Murphy <finnianmurph@gmail.com>
2025-01-21 19:51:59 -0500

    Undid info seperation in preperation to add new functionalites

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-#include "main.h"
+#include <iostream>
+#include <string>
+
+class terminal {
+public:
+int x;
+int y;
+void termDisp(); 
+};
+
 
 
 int main() {
@@ -10,3 +19,34 @@ mTerm.termDisp();
 return 0;
 
 }
+
+
+
+void terminal::termDisp() {
+
+	bool dRun = true;
+	
+	while(dRun == true){
+
+		std::string userInp = "";
+		std::cout 
+		<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+		<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+		<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+		<< 	" Continue? \n  (enter to continue, q to quit): > " << 
+		std::endl;
+		std::cin >> userInp;
+
+		if(userInp == "q"){
+
+			dRun = false;
+
+		} else {}
+
+
+
+	}
+
+
+}
+
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TERMDISP_MAIN_H__
-#define __TERMDISP_MAIN_H__
-
-
-#include "termDisp.h"
-#include "termDisp.cpp"
-using namespace std;
-
-#endif
