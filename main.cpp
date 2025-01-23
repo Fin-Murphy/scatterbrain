bcc2fe18075106a8255d30fff5465066eae8e3d3
Finnian T. Murphy <finnianmurph@gmail.com>
2025-01-23 14:30:04 -0500

    Seperated files back out. Mistake to consolidate in the first place.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <string>
-#include <fstream> 
-#include <vector>
-#include <sstream> 
-#include <iomanip>
-#include <exception>
-#include <stdexcept>	
-//Testing fchanges
-class terminal {
-public:
-int x;
-int y;
-void termDisp(terminal inpterm); 
-std::vector<std::string> fQuery();
-};
+#include "main.h"
 
 
 int main() {
@@ -22,71 +7,6 @@ terminal mTerm;
 std::vector<std::string> tasks = mTerm.fQuery();
 mTerm.termDisp(mTerm);
 
-
 return 0;
-
-}
-
-std::vector<std::string> terminal::fQuery() {
-
-	std::vector<std::string> returnvec = {};
-
-	try {
-
-	std::ifstream file ("./storage.txt");
-	if(!file.is_open()){throw std::invalid_argument("Not a valid arg");}
-	//std::vector<std::string> returnvec = {};
-	std::string line;
-
-		while(std::getline(file,line)) {
-			std::stringstream ss(line);
-			std::string taskName;
-			int taskCval;
-			int taskComp;
-
-		}
-	}
-
-	catch(...){
-		std::cout << "operation failed, creating taskfile" << std::endl;
-
-		std::ofstream file("./storage.txt");
-
-		file << "> SPECIFIER:_Base_Tasklist_File" << std::endl;
-		file << "> 1.2.3.4.5" << std::endl;
-	
-		std::cout << "Wrote in basic file" << std::endl;
-
-    }
-
-return returnvec;
-
-}
-
-
-void terminal::termDisp(terminal inpterm) {
-	bool dRun = true;
-	
-	while(dRun == true){
-
-		std::string userInp = "";
-		std::cout 
-		//<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-		//<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-		//<< "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-		<< 	" Continue? (enter to continue, q to quit): > ";
-		std::cin >> userInp;
-
-		if(userInp == "q"){
-
-			dRun = false;
-
-		}
-		if(userInp == "l"){
-			inpterm.fQuery();
-		}
-
-	}
-
 }
 
